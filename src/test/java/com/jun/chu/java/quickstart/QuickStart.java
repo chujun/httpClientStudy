79719d94039dbbb0f0639f41ab5e14910c26f8a7
jun.chu <1571970439@qq.com>
2017-03-09 14:35:31 +0800

    git init project

---
+++ src/test/java/com/jun/chu/java/quickstart/QuickStart.java
@@ -0,0 +1,56 @@
+package com.jun.chu.java.quickstart;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by chujun on 2017/3/9.
+ */
+public class QuickStart {
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://www.baidu.com/");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        try {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            EntityUtils.consume(entity1);
+        } finally {
+            response1.close();
+        }
+
+
+        HttpPost httpPost = new HttpPost("https://www.baidu.com/");
+        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+        nvps.add(new BasicNameValuePair("username", "vip"));
+        nvps.add(new BasicNameValuePair("password", "secret"));
+        httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+        CloseableHttpResponse response2 = httpclient.execute(httpPost);
+
+        try {
+            System.out.println(response2.getStatusLine());
+            HttpEntity entity2 = response2.getEntity();
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            EntityUtils.consume(entity2);
+        } finally {
+            response2.close();
+        }
+
+
+    }
+}
